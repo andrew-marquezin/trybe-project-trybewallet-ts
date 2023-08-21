46326612287d665f0d60c006b59d7d41c8438f7d
andrew-marquezin <andrewmmarinho@gmail.com>
2023-08-21 05:11:31 -0300

    quinto funcionando

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,5 +1,47 @@
+import { useSelector } from 'react-redux';
+import { ReduxStateType } from '../types';
+
 function Table() {
-  return <div>Table</div>;
+  const { expenses } = useSelector((state: ReduxStateType) => state.wallet);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </tr>
+      </thead>
+      <tbody>
+        {expenses.map((e) => {
+          const cambio = Number(e.exchangeRates[e.currency].ask);
+          return (
+            <tr key={ e.id }>
+              <td>{e.description}</td>
+              <td>{e.tag}</td>
+              <td>{e.method}</td>
+              <td>{Number(e.value).toFixed(2)}</td>
+              <td>{e.exchangeRates[e.currency].name}</td>
+              <td>{Number(e.exchangeRates[e.currency].ask).toFixed(2)}</td>
+              <td>{(cambio * Number(e.value)).toFixed(2)}</td>
+              <td>Real</td>
+              <td>
+                <button>i</button>
+                <button>x</button>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
 
 export default Table;
